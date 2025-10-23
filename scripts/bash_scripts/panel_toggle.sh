ff6ef5dc6ac441e5379b49cfa5c855a040d0fcae
ekhadley <ekhadley@gmail.com>
2025-10-22 22:20:08 -0500

    sysinfo widget

--- scripts/bash_scripts/panel_toggle.sh
+++ scripts/bash_scripts/panel_toggle.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 active_screen=$(hyprctl -j activeworkspace | jq -r '.monitorID')
+
+system_info_str=$(fastfetch -c /home/ek/.config/eww/panel/fastfetch-config.jsonc)
+# echo $system_info_str
+eww update system_info_str="$system_info_str"
+# eww update system_info_str="abc123"
 eww open --toggle panel_window --screen "$active_screen"
